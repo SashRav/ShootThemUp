23ed8a9a5289a7957d0c3232f130d0bf45fe26d9
SashRav <sasha_ravinsky@tut.by>
2021-02-27 17:20:54 +0300

    Added clang file for code formating

--- Source/ShootThemUp/Private/STUGameModeBase.cpp
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -1,5 +1,15 @@
 // Shoot Them Up game. All Rights Resived.
 
-
 #include "STUGameModeBase.h"
 
+void ASTUGameModeBase::BeginPlay()
+{
+    int32 Variable = 0;
+    if (Variable < 10)
+    {
+        for (int32 i = 0; i < 10; ++i)
+        {
+            UE_LOG(LogTemp, Display, TEXT("index: %i"), i);
+        }
+    }
+}
