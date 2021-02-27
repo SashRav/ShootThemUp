95bece892c4da20a09eb77dcd8608e443fa0e050
SashRav <sasha_ravinsky@tut.by>
2021-02-27 18:47:42 +0300

    Lesson 26. Camera controlling by mouse

--- Source/ShootThemUp/Public/Player/STUBaseCharacter.h
+++ Source/ShootThemUp/Public/Player/STUBaseCharacter.h
@@ -7,6 +7,7 @@
 #include "STUBaseCharacter.generated.h"
 
 class UCameraComponent;
+class USpringArmComponent;
 
 UCLASS()
 class SHOOTTHEMUP_API ASTUBaseCharacter : public ACharacter
@@ -18,6 +19,10 @@ public:
     ASTUBaseCharacter();
 
 protected:
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    USpringArmComponent* SpringArmComponent;
+
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
     UCameraComponent* CameraComponent;
 
@@ -34,4 +39,8 @@ public:
 private:
     void MoveForward(float Amount);
     void MoveRight(float Amount);
+
+    //void LookUp(float Amount);
+    //void TurnAround(float Amount);
+
 };
