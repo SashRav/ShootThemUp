8e6f3840943085f9afec4af4dcd226333c80d269
SashRav <sasha_ravinsky@tut.by>
2021-03-27 23:02:17 +0300

    Lesson 43. Migrated Rifel and Launcher from Shooter game. Created Weapon class. Attached it to the actor

---
+++ Source/ShootThemUp/Private/Weapon/STUBaseWeapon.cpp
@@ -0,0 +1,17 @@
+
+#include "Weapon/STUBaseWeapon.h"
+#include "Components/SkeletalMeshComponent.h"
+
+ASTUBaseWeapon::ASTUBaseWeapon()
+{
+
+    PrimaryActorTick.bCanEverTick = false;
+
+    WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>("WeaponMesh");
+    SetRootComponent(WeaponMesh);
+}
+
+void ASTUBaseWeapon::BeginPlay()
+{
+    Super::BeginPlay();
+}
