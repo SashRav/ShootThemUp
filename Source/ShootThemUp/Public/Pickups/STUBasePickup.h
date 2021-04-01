fb67287905ec1c188a2f6aa98a7555ac5774ab16
SashRav <sasha_ravinsky@tut.by>
2021-04-01 20:11:56 +0300

    Lesson 75. Pickup respawn

--- Source/ShootThemUp/Public/Pickups/STUBasePickup.h
+++ Source/ShootThemUp/Public/Pickups/STUBasePickup.h
@@ -11,21 +11,28 @@ class USphereComponent;
 UCLASS()
 class SHOOTTHEMUP_API ASTUBasePickup : public AActor
 {
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	ASTUBasePickup();
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this actor's properties
+    ASTUBasePickup();
 
 protected:
     UPROPERTY(VisibleAnywhere, Category = "Pickup")
     USphereComponent* CollisionComponent;
 
-	virtual void BeginPlay() override;
+    virtual void BeginPlay() override;
     virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickup")
+    float RespawnTime = 5.0f;
 
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+private:
+    virtual bool GivePickupTo(APawn* PlayerPawn);
+    void PickupWasTaken();
+    void Respawn();
 };
