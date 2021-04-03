7583877a66e8294c0837fd1a67a93bc3d0de7da1
SashRav <sasha_ravinsky@tut.by>
2021-04-03 16:55:30 +0300

    Lesson 86. Camera shake

--- Source/ShootThemUp/Public/Components/STUHealthComponent.h
+++ Source/ShootThemUp/Public/Components/STUHealthComponent.h
@@ -7,6 +7,8 @@
 #include "STUCoreTypes.h"
 #include "STUHealthComponent.generated.h"
 
+class UCameraShakeBase;
+
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class SHOOTTHEMUP_API USTUHealthComponent : public UActorComponent
 {
@@ -45,6 +47,10 @@ protected:
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Heal", meta = (EditCondition = "AutoHeal"))
     float HealModifire = 5.0f;
 
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "VFX")
+    TSubclassOf<UCameraShakeBase> CameraShake;
+
+
     virtual void BeginPlay() override;
 
 private:
@@ -57,4 +63,6 @@ private:
 
     void HealUpdate();
     void SetHealth(float NewHealth);
+
+    void PlayCameraShake();
 };
